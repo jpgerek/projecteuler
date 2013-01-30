417a060fe68f870ff14ee2517597d4366d4d9805
jpgerek <juanpablo@guereca.com>
2013-01-29 17:05:15 -0800

    Moving function is_palindromic to euler module

--- p4.py
+++ p4.py
@@ -1,7 +1,4 @@
 import itertools
-
-def is_palindromic(num):
-	str_num = str(num)
-	return str_num == ''.join(reversed(str_num))
+from euler import is_palindromic
 
 print max(a*b for a in xrange(100,1000) for b in xrange(100,1000) if is_palindromic(a*b))
